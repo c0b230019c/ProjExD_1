98de66d41c067de1c18f05c14dfe6605d8b97aec
crow_ak <c0b230019c@edu.teu.ac.jp>
2024-09-24 15:58:09 +0900

    Practice7_1 背景flip

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,22 @@ def main():
     screen = pg.display.set_mode((800, 600)) #ウインドウのサイズを設定(横, 縦)
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #画像をパスを指定して読み込む
+    fbg_img = pg.transform.flip(bg_img, True, False)
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%800)
+        x = -(tmr%1600)
         screen.blit(bg_img, [x, 0]) #自身に別のSurdaceを貼り付ける
+        screen.blit(fbg_img, [x+1600, 0])
         screen.blit(kt_img, [200, 300])
         pg.display.update()
         #if tmr >= 800:
         #    tmr = 0
-        tmr += 1     
+        #tmr += 1
+        print(tmr)    
         clock.tick(200)
 
 
