a1ba1c311ccb057ed7735b772c74c0b4a957a999
crow_ak <c0b230019c@edu.teu.ac.jp>
2024-09-24 15:27:44 +0900

    Practice2

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,14 @@ def main():
     screen = pg.display.set_mode((800, 600)) #ウインドウのサイズを設定(横, 縦)
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #画像をパスを指定して読み込む
+    kt_img = pg.image.load("fig/3.png")
+    kt_img = pg.transform.flip(kt_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #
+        screen.blit(bg_img, [0, 0]) #自身に別のSurdaceを貼り付ける
         pg.display.update()
         tmr += 1        
         clock.tick(10)
