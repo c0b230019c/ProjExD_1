44778f3f1c00938c87bc05f096ae0a18ec04cd24
crow_ak <c0b230019c@edu.teu.ac.jp>
2024-09-24 16:07:05 +0900

    Practice7_2 背景無限ループ

--- pygame_image.py
+++ pygame_image.py
@@ -17,15 +17,17 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%1600)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0]) #自身に別のSurdaceを貼り付ける
         screen.blit(fbg_img, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(fbg_img, [x+4800, 0])
         screen.blit(kt_img, [200, 300])
         pg.display.update()
         #if tmr >= 800:
         #    tmr = 0
-        #tmr += 1
-        print(tmr)    
+        tmr += 1
+        #print(tmr)    
         clock.tick(200)
 
 
